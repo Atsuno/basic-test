fc502ed79ff653cc3e45fd3520296e1172d4410a
Atsuno <achiptsuno@gmail.com>
2017-03-04 10:19:47 +0700

    create Hero

---
+++ src/Models/Hero.js
@@ -0,0 +1,37 @@
+export default class Hero {
+  constructor() {
+    this.name = 'newHero'
+    this.hp = 100
+    this.damage = 10
+    this.weak = false
+  }
+  setName() {
+
+  }
+
+  getName() {
+
+  }
+
+  setHp() {
+
+  }
+
+  getHp() {
+
+  }
+
+  setDamage() {
+
+  }
+
+  getDamage() {
+
+  }
+  setWeak() {
+
+  }
+  isWeak() {
+
+  }
+}
